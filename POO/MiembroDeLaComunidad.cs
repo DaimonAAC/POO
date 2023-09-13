46309883895c0b26dce32f68afc453e0e917ff1b
Daimon Aquino <daimonaquino0228@gmail.com>
2023-09-12 21:48:13 -0400

    Add project files.

---
+++ POO/MiembroDeLaComunidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class  MiembroDeLaComunidad
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int Edad { get; set; }
+        public bool Genero { get; set; }
+
+        public static void reunirse() { }
+    }
+}
